e10aed6a0c38ad932c50a666b54d79afa82526a2
Ahmad H. Daoud <145913339+ahmaddaoud2003@users.noreply.github.com>
2025-08-30 20:27:13 +0200

    Adding Comments

--- ComputingDubinsCruves.py
+++ ComputingDubinsCruves.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-# Unnecessary shortcuts for numpy functions and constant (simplifies the code)
+# Unnecessary shortcuts for numpy functions and constant --> simplifies the code
 cos = np.cos
 sin = np.sin
-norm = np.linalg.norm
+norm = np.linalg.norm        # Euclidean Distance
 atan2 = np.arctan2
 r2d = np.rad2deg
 cross = np.cross
@@ -16,25 +16,29 @@ pi = np.pi
 acos = math.acos
 
 
-#return Dubin's path
-def returnDubinsPath(x1, y1, psi1, x2, y2, psi2, R):
-    # find the unit vector of initial and final headings
-    u_vec1 = np.array([cos(psi1), sin(psi1)])
-    u_vec2 = [x2-x1, y2-y1]
-    u_vec2 = u_vec2/norm(u_vec2)
-    u_vec3 = np.array([cos(psi2), sin(psi2)])
 
-    # calculate cross products to determine turn types
+def returnDubinsPath(x1, y1, psi1, x2, y2, psi2, R):
+    # Find the unit vector of initial and final headings
+    u_vec1 = np.array([cos(psi1), sin(psi1)]) # Direction vector of the robot's start pose
+    u_vec2 = [x2-x1, y2-y1]  # Direction vector of straight-line between start and goal
+    u_vec2 = u_vec2/norm(u_vec2) # Normalizing for the vector to become a direction vector (not position difference) --> sign will tell left/right
+    u_vec3 = np.array([cos(psi2), sin(psi2)]) # Direction vector of the robot's pose at the end goal
+
+    # Calculate cross products which are the direction centers to determine turn types
     cp1 = cross(u_vec1, u_vec2)
     cp2 = cross(u_vec2, u_vec3)
     
-    # determine turn types (CSC or CCC) based on cross products
     sigma1, sigma2, turnType = findCSCTurnType(cp1, cp2)
     xc1, yc1 = returnCircleCenter(x1, y1, psi1, sigma1, R)
     xc2, yc2 = returnCircleCenter(x2, y2, psi2, sigma2, R)
     
-    #Check the distance between circle centers to determine trajectory type
-    if distance(xc1, xc2, yc1, yc2) >= 3*R:
+    '''
+    Determining Trajectory according to the following cases
+    Case 1: Distance (between arcs) > 3*R, there will not be enough space to connect a straight line (Circles (Arcs) might overlap)
+    Case 2: 2*R < Distance  < 3*R: CCC is valid but only if the heading and orientation are valid
+    Case 3: Distance < 2*R: No valid Dubins' Paths
+    '''
+    if distance(xc1, xc2, yc1, yc2) >= 3*R: 
         xtrajectory, ytrajectory, pathDistance = computeCSCTrajectory(x1, x2, xc1, xc2, y1, y2, yc1, yc2, psi1, psi2, R, turnType, sigma1, sigma2)
     elif distance(xc1, xc2, yc1, yc2) < 3*R and distance(xc1, xc2, yc1, yc2) >= 2*R:
         sigma1, sigma2, turn, proceed = findCCCTurnType(turnType)
@@ -51,7 +55,6 @@ def returnDubinsPath(x1, y1, psi1, x2, y2, psi2, R):
     return xtrajectory, ytrajectory, pathDistance
         
         
-#Determine CSC turn types based on cross products
 def findCSCTurnType(cp1, cp2):
     if (cp1 <= 0 and cp2 > 0):
         turn = "RSL"
@@ -74,7 +77,7 @@ def findCSCTurnType(cp1, cp2):
         sigma2 = 1
     return sigma1, sigma2, turn
 
-#Determine CCC turn types based on cross product
+
 def findCCCTurnType(turnOld):
     proceed = False
     sigma1 = 0
@@ -92,24 +95,23 @@ def findCCCTurnType(turnOld):
         proceed = True
     return sigma1, sigma2, turn, proceed
 
-# Return the center of turning circle
 def returnCircleCenter(x, y, psi, sigma, R):
-    #update xc and yc
     xc = x + R * cos(psi + sigma * pi / 2)
     yc = y + R * sin(psi + sigma * pi / 2)
     return xc, yc
 
-#Calculate the euclidean distance between the two centers
 def distance(x1, x2, y1, y2):
     return sqrt((y2-y1)**2 + (x2-x1)**2)
 
-#Compute the CSCtrajectory
 def computeCSCTrajectory(x1, x2, xc1, xc2, y1, y2, yc1, yc2, psi1, psi2, R, turnType, sigma1, sigma2):
-    #find heading and distance
     psiL = atan2(yc2-yc1, xc2-xc1)    
     Stan = distance(xc1, xc2, yc1, yc2)
 
-    # find intermediate heading angle based on angle psiD based on turn type
+    '''
+    Find intermediate heading angle based on angle psiD based on turn type
+    Case 1: RSR & LSL have the same direction --> The straight line between them will be an external tangent
+    Case 2: RSL & LSR have opposite directios --> The straight line between them will be an internal tangent (Diagonal)
+    '''
     if turnType == "RSR" or turnType == "LSL":
         psiD = psiL
     elif turnType == "LSR":
@@ -121,7 +123,6 @@ def computeCSCTrajectory(x1, x2, xc1, xc2, y1, y2, yc1, yc2, psi1, psi2, R, turn
         if psiD < -pi:
             psiD = 2 * pi - abs(psiD)
         
-    #initialize path distance and trejectory lists
     pathDistance = 0
     xt1 = [x1]
     yt1 = [y1]
@@ -130,7 +131,15 @@ def computeCSCTrajectory(x1, x2, xc1, xc2, y1, y2, yc1, yc2, psi1, psi2, R, turn
     psiT = psi1
     i = 1
 
-    # calculate the trajectory for the first turn segment
+    '''
+    The loop will run until the trajecory heading is within 0.1 of the desired heading
+    MCI is a 2D rotation matrix the increments points around the circle center by increments of 0.05 Rad ~ 3 degrees
+    vec2 is a vector from the circle center to the starting point, we multiply this with rotation matrix 
+    The points that came from the product are added to the trajectory (waypoint list), and we update the tajectory heading (0.05 from rotation matrix)
+    The first while loop is for the first trun segment and the second while loop is for the second turn segement
+    The first loop moves forward along the first arc (incrementing heading, the second loop moves backward decrementing heading
+    After generating two arcs, we just connect them with a straight line
+    '''
     while (not psiD - 0.1 < psiT < psiD + 0.1):
         i = i + 1
         Mco = np.array([[cos(0.05*i),-sigma1*sin(0.05*i)],[sigma1*sin(0.05*i),cos(0.05*i)]])
@@ -139,34 +148,31 @@ def computeCSCTrajectory(x1, x2, xc1, xc2, y1, y2, yc1, yc2, psi1, psi2, R, turn
         xt1.append(xco[0])
         yt1.append(yco[0])
 
-        # Update path distance and psiT
         pathDistance = pathDistance + R * (0.05)
         psiT = psiT + sigma1 * 0.05
 
-        # Adjust psiT if it falls below -pi or exceeds pi
+        # Keeps psiT between [-pi, pi]
         if (psiT < -pi):
             psiT = 2 * pi - abs(psiT)
         if (psiT > pi):
             psiT = psiT - 2 * pi
 
-    # calculate the straight segment trajectory between the two turns
     xt2 = [x2]
     yt2 = [y2]
     xci = [x2]
     yci = [y2]
     psiT = psi2
     i = 1
-    # generate a trajectory from cut-in point to the end
-    while (not psiD - 0.1 < psiT < psiD + 0.1):
+    while (not psiD - 0.1 < psiT < psiD + 0.1): 
         i = i + 1
         Mci = np.array([[cos(0.05*i),sigma2*sin(0.05*i)],[-sigma2*sin(0.05*i),cos(0.05*i)]])
         vec2 = np.array([[x2 - xc2],[y2 - yc2]])
         [xci, yci] =  Mci @ vec2 + np.array([[xc2],[yc2]])   
         xt2.insert(0,xci[0])
         yt2.insert(0,yci[0])
-        pathDistance = pathDistance + R*(0.1)
+        pathDistance = pathDistance + R*(0.05)
         psiT = psiT - sigma2*0.05
-        #Adjust psiT if it falls below -pi or exceeds pi
+        # Keeps psiT between [-pi, pi]
         if (psiT < -pi):
             psiT = 2 * pi - abs(psiT)
         if (psiT > pi):
@@ -177,7 +183,6 @@ def computeCSCTrajectory(x1, x2, xc1, xc2, y1, y2, yc1, yc2, psi1, psi2, R, turn
     v = np.array([xci[0] - xco[0], yci[0] - yco[0]])
     u_hat = v/norm(v)
     dist = distance(xci[0], xco[0], yci[0], yco[0])
-    # Generate straight trajectory between cut-out and cut-in points
     for j in range(int(dist/3)):
         xtrajectory.append(xtrajectory[-1] + 3*u_hat[0])
         ytrajectory.append(ytrajectory[-1] + 3*u_hat[1])
@@ -188,7 +193,12 @@ def computeCSCTrajectory(x1, x2, xc1, xc2, y1, y2, yc1, yc2, psi1, psi2, R, turn
     pathDistance = pathDistance + dist
     return xtrajectory, ytrajectory, pathDistance
 
-# Compute CCC trajectory
+'''
+This step is very similar to compsuting CSC curves
+We compute the circle center of the intermediate circle (arc), then the center of first circle and then second.
+We use the rotation matrix to find the arcs of each circle
+Then we just connect the arcs 
+'''
 def computeCCCTrajectory(x1, x2, xc1, xc2, y1, y2, yc1, yc2, R, sigma1, sigma2):
     Stan = distance(xc1, xc2, yc1, yc2)
     # Calculate intermediate circle center
