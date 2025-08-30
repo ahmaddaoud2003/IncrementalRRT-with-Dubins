35af87cf7cdf15c99b05e52fde959e73b7ec407d
Ahmad H. Daoud <145913339+ahmaddaoud2003@users.noreply.github.com>
2025-08-30 21:11:40 +0200

    Better Clarity

--- IncrementalRRTDubinsPlotting.py
+++ IncrementalRRTDubinsPlotting.py
@@ -13,15 +13,14 @@ grid = np.load('cspaceRoadmap.npy')
 fig = plt.figure("RRT Driving Algorithm")
 plt.imshow(grid, cmap='binary', origin = 'lower')
 
-#Function to find near target points and the heading setpoints
+
 def findNearTarget(i):
     xLocation = [10, 250, 500, 750, 1000, 1250, 1500, 1750, 1990]
     yLocation = [600, 365, 189, 254, 425, 675, 725, 675, 425]
-    headingSetpoint = [-0.7854, -0.7854, 0, 0.7854, 0.7854, 0.7854, 0, -0.7854, -0.7854]
+    headingSetpoint = [-0.8, -0.8, 0, 0.8, 0.8, 0.8, 0, -0.8, -0.8]
     return xLocation[i], yLocation[i], headingSetpoint[i]
 
 
-#Initial starting points and headings
 X = 5
 Y = 600
 start = np.array([X,Y])
@@ -30,7 +29,11 @@ turnRadius = 25
 numIterations = 50
 rrTrees = []
 
-#Loop through 8 target points
+'''
+This is the backbone of the Incremental part.
+We set the goal as the next corresponding points the Location arrays
+When we reach the target it becomes the the new starting point and target point becomes the next one, we repaet iteratively until the end
+'''
 for i in range(8):
     xTarget, yTarget, psiTarget = findNearTarget(i+1)
     plt.plot(xTarget, yTarget, marker="x", markersize=8, markeredgecolor="g", markerfacecolor="g")
